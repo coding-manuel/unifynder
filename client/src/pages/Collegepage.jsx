5813219e4e28c3195b2f866548199bd31d44744f
[JyothisShajan] <[jyothisshajan41@gmail.com]>
2022-03-15 17:39:27 +0530

    college page finished

---
+++ client/src/pages/Collegepage.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Collegecomponent from '../components/Collegecomponent';
+import Footer from '../components/Footer/Footer';
+
+export default function Collegepage() {
+  return (
+      <>
+    <Navbar/>
+    <Collegecomponent/>
+    <Footer/>
+    </>
+  
+  );
+}
